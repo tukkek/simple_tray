30f8b6198c94d3b7513778236a910608ca97e053
Alex Henry <tukkek@gmail.com>
2024-06-27 00:00:00 +0000

    Use PyQT6

--- tray.py
+++ tray.py
@@ -1,15 +1,15 @@
-import PyQt5.QtGui,PyQt5.QtWidgets,subprocess
+import PyQt6.QtGui,PyQt6.QtWidgets,subprocess
 
 class Tray:
-  def __init__(self,name,icon,interval):
-    self.application=PyQt5.QtWidgets.QApplication([])
-    self.icon=PyQt5.QtWidgets.QSystemTrayIcon()
-    self.menu=PyQt5.QtWidgets.QMenu()
-    self.timer=PyQt5.QtCore.QTimer()
+  def __init__(self,name,icon,interval=1):
+    self.application=PyQt6.QtWidgets.QApplication([])
+    self.icon=PyQt6.QtWidgets.QSystemTrayIcon()
+    self.menu=PyQt6.QtWidgets.QMenu()
+    self.timer=PyQt6.QtCore.QTimer()
     self.said=False
     self.name=name
-    self.icon.activated.connect(lambda:self.menu.popup(PyQt5.QtGui.QCursor.pos()))
-    self.icon.setIcon(PyQt5.QtGui.QIcon(icon))
+    self.icon.activated.connect(lambda:self.menu.popup(PyQt6.QtGui.QCursor.pos()))
+    self.icon.setIcon(PyQt6.QtGui.QIcon(icon))
     self.icon.setContextMenu(self.menu)
     self.timer.timeout.connect(self.update)
     self.timer.setInterval(interval*1000)
@@ -25,7 +25,7 @@ class Tray:
     raise Exception('Unimplemented update method.')
     
   def start(self):
-    q=PyQt5.QtWidgets.QAction()
+    q=PyQt6.QtGui.QAction()
     q.triggered.connect(self.application.quit) 
     q.setText('Quit') 
     self.menu.addAction(q)
